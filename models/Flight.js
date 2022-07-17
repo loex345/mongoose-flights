2ef149cf7f5ade1c35cc4886dcbb34a12504dea7
loex345 <maitlandeg@gmail.com>
2022-07-17 14:55:21 -0700

    Server.js

--- models/Flight.js
+++ models/Flight.js
@@ -1,9 +1,23 @@
 const mongoose = reguire('mongoose');
 const Schema = mongoose.Schema;
 
+
 const flightSchema = new mongoose.Schema({
-    airline: String,
-    airport: String,
+    airline:{
+    type:String,
+    enum:['American','Southwest' & 'United']
+    },
+    airport:{
+     type:String,
+     enum:['AUS','DFW','DEN','LAX','SAN'],
+     default:'Den'
+    },
     flightNo: Number,
-    departs: Date
+    departs:{
+      type:Date,
+      default: function() { 
+      return new Date().getFullYear();
+    },
+  }
+
 });
